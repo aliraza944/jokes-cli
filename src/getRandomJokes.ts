3470206da96f8295ea3eeb3d0923e34893bf2a4c
aliraza944 <alisabri022@gmail.com>
2023-03-14 22:31:24 +0500

    removed the spinner and boxen logic to the native function form the index file

--- src/getRandomJokes.ts
+++ src/getRandomJokes.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
 import chalk from "chalk";
+import ora from "ora";
+import boxen from "boxen";
 const getRandomJokes = async () => {
+  const spinner = ora("Loading").start();
+  spinner.color = "yellow";
   try {
     const { data } = await axios.get(
       "https://official-joke-api.appspot.com/jokes/random"
     );
-
-    return `
+    if (data) {
+      spinner.stop();
+      const joke = `
     ${chalk.green(data.setup)}
     
     ${chalk.yellow.bgBlack.bold(data.punchline)}
     `;
+      console.log(
+        boxen(joke, { padding: 2, title: "Jokes", titleAlignment: "center" })
+      );
+    }
   } catch (error) {
     throw new Error("Error fetching jokes");
   }
