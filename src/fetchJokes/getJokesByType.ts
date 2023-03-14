f90e7d1625dc3684b6846c3b65e0197476c1ad0f
aliraza944 <alisabri022@gmail.com>
2023-03-14 23:23:23 +0500

    this will add the proper type saftey to fetch jokes by type and add standard boxen

--- src/fetchJokes/getJokesByType.ts
+++ src/fetchJokes/getJokesByType.ts
@@ -1,16 +1,28 @@
 import axios from "axios";
 import chalk from "chalk";
-const getJokesByType = async (type: string): Promise<string> => {
+import boxen from "boxen";
+import ora from "ora";
+type JokeType = "general" | "programming" | "knock-knock";
+
+const getJokesByType = async (type: JokeType): Promise<void> => {
+  const spinner = ora("Loading").start();
+  spinner.color = "yellow";
   try {
     const { data } = await axios.get(
       `https://official-joke-api.appspot.com/jokes/${type}/random`
     );
 
-    return `
-    ${chalk.green(data.setup)}
+    if (data) {
+      spinner.stop();
+      const joke = `
+    ${chalk.green(data[0].setup)}
     
-    ${chalk.yellow.bgBlack.bold(data.punchline)}
+    ${chalk.yellow.bgBlack.bold(data[0].punchline)}
     `;
+      console.log(
+        boxen(joke, { padding: 2, title: "Jokes", titleAlignment: "center" })
+      );
+    }
   } catch (error) {
     throw new Error("Error fetching jokes");
   }
