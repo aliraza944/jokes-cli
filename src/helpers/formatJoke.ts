deeb8792f3155f2c8c08d1ec8ee749a5fcdbff97
aliraza944 <alisabri022@gmail.com>
2023-03-15 21:32:30 +0500

    created a helper method that will formate the jokes

---
+++ src/helpers/formatJoke.ts
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+import boxen from "boxen";
+
+const formatJoke = (setup: string, punchline: string) => {
+  const joke = `
+    ${chalk.green(setup)}
+    
+    ${chalk.yellow.bgBlack.bold(punchline)}
+    `;
+  console.log(
+    boxen(joke, { padding: 2, title: "Joke", titleAlignment: "center" })
+  );
+};
+
+export default formatJoke;
